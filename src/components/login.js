7dfbaeb883fea873c84756009610756acdabd236
Ravindu Hasantha <ravindu.h@eyepax.com>
2019-02-23 21:16:26 +0530

    react-17 react-18 react-19 react-20

--- src/components/login.js
+++ src/components/login.js
@@ -7,16 +7,95 @@ class Login extends Component {
         super(props);
         this.state = {
             showLogin: this.props.showLogin,
-            isLoading: false
+            isLoading: false,
+            formData: {
+                username: '',
+                password: ''
+            },
+            passwordRequired: false,
+            usernameRequired: false,
         };
         this.textInput = React.createRef();
+        this.passwordInput = React.createRef();
     }
 
+    //Lfie cycle methods-----------------------
     componentDidUpdate() {
-        this.textInput.current.focus();
+        //If both these are empty focus on username
+        if (this.state.formData.password == '' && this.state.formData.username == '') {
+            this.textInput.current.focus();
+        }
+    }
+    
+
+    //custom functions-------------------------
+    handleChange = event => {
+        const { formData } = this.state;
+      
+        this.setState({
+          formData: {
+            ...formData, // leave other values unchanged
+            [event.target.name]: event.target.value, // update the changed value
+          },
+          errors: []
+        });
+
+        //disabling error mesages if username and password entered
+        if (formData.username != '') {
+            this.setState({
+                usernameRequired: false
+            });
+        }
+
+        if (formData.password != '') {
+            this.setState({
+                passwordRequired: false
+            });
+        }
+    }
+
+    handleSubmit = event => {
+        console.log(this.state);
+        event.preventDefault();
+      
+        const { formData, errors } = this.state;
+        const { username, password } = formData;
+      
+        if (username == '') {
+          this.setState({
+            usernameRequired: true
+          });
+        }
+
+        if (password == '') {
+            this.setState({
+                passwordRequired: true
+              });
+        }
+
+        if (username == '') {
+            this.textInput.current.focus();
+        } else if (password == '') {
+            this.passwordInput.current.focus();
+        }
+        const creds = {username, password}
+    }
+
+    resetForm () {
+        this.setState({
+            isLoading: false,
+            formData: {
+                username: '',
+                password: ''
+            },
+            passwordRequired: false,
+            usernameRequired: false
+        })
+        console.log(this.state);
     }
 
     render () {
+        console.log(this.state.formData.password);
         let open = "";
         const { showLogin, showHideLogin } = this.props;
         if (showLogin) { 
@@ -25,7 +104,7 @@ class Login extends Component {
             return (
                 <div className={"popup " + open}>
                     <div className="popup__header">
-                        <div title="Close" className="close layout--center" onClick={() => showHideLogin(false)}>
+                        <div title="Close" className="close layout--center" onClick={() => {showHideLogin(false); this.resetForm();}}>
                             X
                         </div>
                     </div>
@@ -42,16 +121,30 @@ class Login extends Component {
                                 <form name="login">
                                     <div className="login">
                     
-                                        <input type="text" placeholder="USERNAME" ref={this.textInput}/>
-                                        <input type="password" placeholder="PASSWORD" />
+                                        <input 
+                                            type="text" 
+                                            name="username"
+                                            placeholder="USERNAME" 
+                                            ref={this.textInput}
+                                            onChange={ this.handleChange }
+                                        />
+                                        <input 
+                                            type="password" 
+                                            placeholder="PASSWORD" 
+                                            name="password"
+                                            ref={this.passwordInput}
+                                            onChange={ this.handleChange }
+                                        />
                     
-                                        <Loader isLoading={this.state.isLoading}/>
+                                        <Loader isLoading={ this.state.isLoading }/>
                     
                                         <div className="form__error">
-                                            Some fields are missing !
+                                            {this.state.usernameRequired ? "Username is required" : "" }
+                                            <br />
+                                            {this.state.passwordRequired ? "Password is required" : "" }
                                         </div>
                     
-                                        <input type="submit" value="LET ME IN" />
+                                        <input type="submit" value="LET ME IN" onClick={ this.handleSubmit } />
                                     </div>
                                 </form>
                             </div>
