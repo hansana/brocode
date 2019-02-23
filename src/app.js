78d531fd2ab2f67d983d277051b034114c102040
Ravindu Hasantha <ravindu.h@eyepax.com>
2019-02-23 13:21:51 +0530

    react-6

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from "react-dom";
+import Header from './components/header.js';
 
 import "./styles.css";
 
@@ -11,29 +12,9 @@ class App extends Component {
         
         {/* <!-- Start of Header -->
         <!-- ======================= --> */}
+            <Header />
         
-        <section className="header layout--center">
-            <div className="header__content layout--wrapped">
-                <div className="brand">
-                    <a href="/"><div className="brand__name"><span>#</span>DEVRANT</div></a>
-                </div>
-        
-                {/* <!-- User Profile --> */}
-                <div className="profile layout--center">
-                    <div className="profile__picture">
-                        <svg height="36" width="36">
-                            <circle cx="18" cy="18" r="18" fill="#5c5f6f"></circle>
-                        </svg>
-                    </div>
-                    <div className="profile__name">broCode</div>
-                </div>
-        
-                <div className="join">
-                    {/* <!-- <span>Join</span> --> */}
-                    <span>Sign Out</span>
-                </div>
-            </div>
-        </section>
+
         
         {/* <!-- ======================= -->
         <!-- End of Header -->
